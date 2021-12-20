00de8ee863194ea1ca3a949e5ad56ca4e3e1b271
Haywire <rdewolf@telenet.be>
2021-12-20 17:11:17 +0100

    Fixed skip button and performed small refactor

---
+++ includes/WhiteKey.py
@@ -0,0 +1,13 @@
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle, Color
+
+class WhiteKey(Widget):
+    def __init__(self, **kwargs):
+        super(WhiteKey, self).__init__(**kwargs)
+        self.col = (1,1,1,1)
+        self.originalCol = self.col
+        with self.canvas:
+            self._color = Color(*self.col)
+            self._rect = Rectangle(pos=self.pos,size=(23,150))
+    def update(self):
+        self._color.rgba = self.col
