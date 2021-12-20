f1065056fbf471fc9c10e69be21244656a571e37
Haywire <rdewolf@telenet.be>
2021-12-20 17:24:19 +0100

    another small update

--- includes/BlackKey.py
+++ includes/BlackKey.py
@@ -4,8 +4,7 @@ from kivy.graphics import Rectangle, Color
 class BlackKey(Widget):
     def __init__(self, **kwargs):
         super(BlackKey, self).__init__(**kwargs)
-        self.col = (0,0,0,1)
-        self.originalCol = self.col
+        self.originalCol = (0,0,0,1)
         with self.canvas:
-            self._color = Color(*self.col)
+            self._color = Color(*self.originalCol)
             self._rect = Rectangle(pos=self.pos,size=(12,100))
--- includes/WhiteKey.py
+++ includes/WhiteKey.py
@@ -4,8 +4,7 @@ from kivy.graphics import Rectangle, Color
 class WhiteKey(Widget):
     def __init__(self, **kwargs):
         super(WhiteKey, self).__init__(**kwargs)
-        self.col = (1,1,1,1)
-        self.originalCol = self.col
+        self.originalCol = (1,1,1,1)
         with self.canvas:
-            self._color = Color(*self.col)
+            self._color = Color(*self.originalCol)
             self._rect = Rectangle(pos=self.pos,size=(23,150))
